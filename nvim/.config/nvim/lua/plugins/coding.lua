71c6a1bae4c96763a474fa777976fe6a2eeb9ef0
Tommy Guo <tommyguo024@outlook.com>
2026-03-02 19:56:57 -0500

    feat(nvim): add jupytor notebook support

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -77,4 +77,33 @@ return {
             })
         end
     },
+
+    {
+        "3rd/image.nvim",
+        opts = {
+            max_width = 100,
+            max_height = 12,
+            max_width_window_percentage = math.huge,
+            max_height_window_percentage = math.huge,
+            window_overlap_clear_enabled = true,
+            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+        }
+    },
+
+    {
+        "benlubas/molten-nvim",
+        version = "^1.0.0",
+        build = ":UpdateRemotePlugins",
+        init = function()
+            vim.g.molten_image_provider = "image.nvim"
+            vim.g.molten_output_win_max_height = 20
+        end,
+        keys = {
+            { "<leader>mi", ":MoltenInit<CR>",                desc = "Initialize Molten" },
+            { "<leader>e",  ":MoltenEvaluateOperator<CR>",    desc = "Evaluate Operator" },
+            { "<leader>rl", ":MoltenEvaluateLine<CR>",        desc = "Evaluate Line" },
+            { "<leader>re", ":<C-u>MoltenEvaluateVisual<CR>", mode = "v",                desc = "Evaluate Visual" },
+            { "<leader>md", ":MoltenDelete<CR>",              desc = "Delete Output" },
+        },
+    },
 }
